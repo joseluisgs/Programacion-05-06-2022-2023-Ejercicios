409468e9052b194abcf11c10ba5177da13c2da0e
SergioDeIscarValera <seroigres888@gmail.com>
2023-02-21 13:45:08 +0100

    Itv-Kotlin v0.1

---
+++ itv/SergioDeIscar-itv-kotlin/SergioDeIscar-itv-kotlin/src/main/kotlin/interfaces/ItvExtension.kt
@@ -0,0 +1,18 @@
+package interfaces
+
+import models.Vehiculo
+
+interface ItvExtension: CrudRepository<Vehiculo, String> {
+    fun getCoches(): List<Vehiculo>
+    fun getCocheMaxModerno(): Vehiculo
+    fun getVehiculoMinKilometros(): Vehiculo
+    fun getMediaKilometrosMotos(): Double
+    fun getOldestCoche(): Vehiculo
+    fun getCountVehiculos(): Map<String, Int>
+    fun getCountVehiculosAptos(): Map<String, Int>
+    fun getMediaVehiculosAnio(): Map<String, Double>
+    fun groupVehiculosByMarca(): Map<String, List<Vehiculo>>
+    fun sortVehiculosByAnio(): List<Vehiculo>
+    fun sortDesVehiculosByMarca(): List<Vehiculo>
+    fun groupVehiculosByMarcaSortByKilometro(): Map<String, List<Vehiculo>>
+}
---
+++ itv/SergioDeIscar-itv-kotlin/SergioDeIscar-itv-kotlin/src/main/kotlin/repositories/ItvRepository.kt
@@ -0,0 +1,82 @@
+package repositories
+
+import interfaces.ItvExtension
+import models.Vehiculo
+
+class ItvRepository(val maxSize: Int): ItvExtension {
+    private val vehiculos = mutableMapOf<String, Vehiculo>()
+    private fun isFull():Boolean{
+        return vehiculos.size >= maxSize
+    }
+
+    override fun getCoches(): List<Vehiculo> {
+        return vehiculos.filterValues { it::class.simpleName == "Coche" }.values.toList()
+    }
+
+    override fun getCocheMaxModerno(): Vehiculo {
+        return vehiculos.values.minBy { it.anio }
+    }
+
+    override fun getVehiculoMinKilometros(): Vehiculo {
+        return vehiculos.values.minBy { it.kilometro }
+    }
+
+    override fun getMediaKilometrosMotos(): Double {
+        return vehiculos.values.map { it.kilometro }.average()
+    }
+
+    override fun getOldestCoche(): Vehiculo {
+        return vehiculos.values.maxBy { it.anio }
+    }
+
+    override fun getCountVehiculos(): Map<String, Int> {
+        return vehiculos.values.groupBy{ it::class.simpleName ?: "_" }.mapValues { it.value.size }
+    }
+
+    override fun getCountVehiculosAptos(): Map<String, Int> {
+        return vehiculos.filterValues { it.apto }.values.groupBy{ it::class.simpleName ?: "_" }.mapValues { it.value.size }
+    }
+
+    override fun getMediaVehiculosAnio(): Map<String, Double> {
+        return vehiculos.values.groupBy{ it::class.simpleName ?: "_" }.mapValues { it.value.map { it.anio.year }.average() }
+    }
+
+    override fun groupVehiculosByMarca(): Map<String, List<Vehiculo>> {
+        return vehiculos.values.groupBy { it.marca }
+    }
+
+    override fun sortVehiculosByAnio(): List<Vehiculo> {
+        return vehiculos.values.sortedBy { it.anio }
+    }
+
+    override fun sortDesVehiculosByMarca(): List<Vehiculo> {
+        return vehiculos.values.sortedByDescending { it.marca }
+    }
+
+    override fun groupVehiculosByMarcaSortByKilometro(): Map<String, List<Vehiculo>> {
+        return vehiculos.values.groupBy { it.marca }.mapValues { it.value.sortedBy { it.kilometro } }
+    }
+
+    override fun save(t: Vehiculo): Vehiculo? {
+        if (isFull()) return null
+        vehiculos[t.matricula] = t
+        return t
+    }
+
+    override fun find(id: String): Vehiculo? {
+        return vehiculos[id]
+    }
+
+    override fun delete(id: String): Vehiculo? {
+        return vehiculos.remove(id)
+    }
+
+    override fun getAll(): List<Vehiculo> {
+        return vehiculos.values.toList()
+    }
+
+    override fun saveAll(list: List<Vehiculo>) {
+        if (isFull()) return
+        list.forEach{ save(it) }
+    }
+}
